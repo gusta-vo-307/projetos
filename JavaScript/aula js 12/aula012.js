caf879084956d6c5fd892f55447e2d9d6a2c31f8
gusta-vo-307 <gugaalves307@gmail.com>
2023-10-30 19:23:55 -0300

    commit dia 30/10

---
+++ JavaScript/aula js 12/aula012.js
@@ -0,0 +1,10 @@
+var idade = 19
+console.log(`voce tem ${idade} anos`)
+if(idade < 16 ){
+    console.log("Você não vota")
+} else if (idade < 18){
+        console.log("Voto opcional")
+} else if (idade >= 18) {
+    console.log("Voto obrigatório")
+}
+    
